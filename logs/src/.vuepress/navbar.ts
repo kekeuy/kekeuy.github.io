f114b326d7ff9fe2538a8df835f4790122120cb7
noodle <15915496031@163.com>
2024-04-05 01:02:04 +0800

    init vuepress

--- logs/src/.vuepress/navbar.ts
+++ logs/src/.vuepress/navbar.ts
@@ -1,36 +1,36 @@
 import { navbar } from "vuepress-theme-hope";
 
-export const zhNavbar = navbar([
-  "/zh/",
-  "/zh/demo/",
+export default navbar([
+  "/",
+  "/demo/",
   {
-    text: "博文",
+    text: "Posts",
     icon: "pen-to-square",
-    prefix: "/zh/posts/",
+    prefix: "/posts/",
     children: [
       {
-        text: "苹果",
+        text: "Apple",
         icon: "pen-to-square",
         prefix: "apple/",
         children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
+          { text: "Apple1", icon: "pen-to-square", link: "1" },
+          { text: "Apple2", icon: "pen-to-square", link: "2" },
           "3",
           "4",
         ],
       },
       {
-        text: "香蕉",
+        text: "Banana",
         icon: "pen-to-square",
         prefix: "banana/",
         children: [
           {
-            text: "香蕉 1",
+            text: "Banana 1",
             icon: "pen-to-square",
             link: "1",
           },
           {
-            text: "香蕉 2",
+            text: "Banana 2",
             icon: "pen-to-square",
             link: "2",
           },
@@ -38,15 +38,15 @@ export const zhNavbar = navbar([
           "4",
         ],
       },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+      { text: "Cherry", icon: "pen-to-square", link: "cherry" },
+      { text: "Dragon Fruit", icon: "pen-to-square", link: "dragonfruit" },
       "tomato",
       "strawberry",
     ],
   },
   {
-    text: "V2 文档",
+    text: "V2 Docs",
     icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
+    link: "https://theme-hope.vuejs.press/",
   },
 ]);
--- logs/src/README.md
+++ logs/src/README.md
@@ -3,9 +3,9 @@ home: true
 layout: BlogHome
 icon: home
 title: Blog Home
-heroImage: https://theme-hope-assets.vuejs.press/logo.svg
-heroText: The name of your blog
-tagline: You can put your slogan here
+heroImage: ''
+heroText: Spring Journal
+tagline: Take a spring stroll
 heroFullScreen: true
 projects:
   - icon: project
